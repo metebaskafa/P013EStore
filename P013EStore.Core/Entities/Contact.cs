0be8c787888272f938ad416277781ff0e3419fa2
metebaskafa <metebaskafa@msn.com>
2023-05-01 23:05:17 +0300

    Add project files.

---
+++ P013EStore.Core/Entities/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace P013EStore.Core.Entities
+{
+    public class Contact:IEntity
+    {
+        public int Id { get; set; }
+       
+        [Display(Name = "Ad")]
+        public string Name { get; set; }
+        [Display(Name = "Soyad")]
+        public string? Surname { get; set; }
+        public string Email { get; set; }
+
+        [Display(Name = "Telefon")]
+        public string? Phone { get; set; }
+        [Display(Name = "Mesajınız")]
+        public string Message { get; set; }
+        [Display(Name = "Eklenme Tarihi"), ScaffoldColumn(false)]
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+        
+    }
+
+}

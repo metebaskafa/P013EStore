0060ac6e10b8a1a94ea254e2839653fc6881bf8d
metebaskafa <metebaskafa@msn.com>
2023-05-31 22:04:45 +0300

    31,05

---
+++ P013EStore.WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P013EStore.Core.Entities;
+using P013EStore.Service.Abstract;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P013EStore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IService<Brand> _service; // readonly nesneler sadece constructor metotda dolduralabilir service e tıklayıp generate constructor diyoruz. 
+
+        public BrandsController(IService<Brand> service)
+        {
+            _service = service;
+        }
+        // GET: api/<BrandsController>
+        [HttpGet]
+        public async Task<IEnumerable<Brand>> GetAsync()
+        {
+            return await _service.GetAllAsync();
+        }
+
+        // GET api/<BrandsController>/5
+        [HttpGet("{id}")]
+        public async Task<Brand> GetAsync(int id)
+        {
+            return await _service.FindAsync(id);
+        }
+
+        // POST api/<BrandsController>
+        [HttpPost]
+        public async Task<Brand> PostAsync([FromBody] Brand value)
+        {
+            await _service.AddAsync(value);
+            await _service.SaveAsync();
+            return value;
+        }
+
+        // PUT api/<BrandsController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(int id, [FromBody] Brand value)
+        {
+            _service.Update(value);
+            int sonuc= await _service.SaveAsync();
+            if (sonuc>0)
+            {
+                return Ok(value);
+            }
+            return StatusCode(StatusCodes.Status304NotModified);
+        }
+
+        // DELETE api/<BrandsController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id) // geriye değer dönmeyeceksek void olur
+        {
+            var kayit = _service.Find(id);
+            if (kayit == null)
+            {
+                return NoContent();
+            }
+            _service.Delete(kayit);
+            _service.Save();
+            return Ok(kayit);
+        }
+    }
+}
---
+++ P013EStore.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P013EStore.Core.Entities;
+using P013EStore.Service.Abstract;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P013EStore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IService<Category> _service;
+
+        public CategoriesController(IService<Category> service)
+        {
+            _service = service;
+        }
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAsync()
+        {
+            return await _service.GetAllAsync();
+        }
+
+        // GET api/<CategoriesController>/5
+        [HttpGet("{id}")]
+        public async Task<Category> GetAsync(int id)
+        {
+            return await _service.FindAsync(id);
+        }
+
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public async Task<int> PostAsync([FromBody] Category value) // from query ?query string
+        {
+            await _service.AddAsync(value);
+            return await _service.SaveAsync();
+           
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut] //  [HttpPut("{id}")] orjinali
+        public async Task<int> PutAsync(int id, [FromBody] Category value)
+        {
+            _service.Update(value);
+            return await _service.SaveAsync();
+           
+        }
+
+        // DELETE api/<CategoriesController>/5
+        [HttpDelete]
+        public async Task<int> Delete([FromBody] Category value)
+        {
+          _service.Delete(value);
+            return await _service.SaveAsync();
+        }
+    }
+}
